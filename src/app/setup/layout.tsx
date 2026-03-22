5a739ae05178ff954df157254ff762c2beb85339
Mike Tobias <m@mctech.llc>
2026-03-21 18:56:49 -0700

    feat: redirect to callbackUrl after onboarding completion

--- src/app/setup/layout.tsx
+++ src/app/setup/layout.tsx
@@ -1,5 +1,10 @@
+import { Suspense } from "react"
 import { AuthSessionProvider } from "@/components/session-provider"
 
 export default function SetupLayout({ children }: { children: React.ReactNode }) {
-  return <AuthSessionProvider>{children}</AuthSessionProvider>
+  return (
+    <AuthSessionProvider>
+      <Suspense>{children}</Suspense>
+    </AuthSessionProvider>
+  )
 }
